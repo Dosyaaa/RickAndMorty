5a435c18995793559b6a432450709bb47399e6c2
kamila <murtazaeva.kamila01@gmail.com>
2023-04-14 17:41:34 +0600

    added colors

---
+++ RickAndMorty/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  RickAndMorty
+//
+//  Created by Kamila on 12/4/23.
+//
+
+import UIKit
+
+enum AppBackgroundColor {
+    static let backgroundColor = UIColor(cgColor: CGColor(red: 20/255, green: 19/255, blue: 20/255, alpha: 1))
+}
+
+enum ButtonAppearance {
+    static let backgroundColor = UIColor(cgColor: CGColor(red: 118/255, green: 208/255, blue: 42/255, alpha: 1))
+    static let textColor = UIColor(cgColor: CGColor(red: 20/255, green: 19/255, blue: 20/255, alpha: 1))
+}
