af37638be0a0e55fb812c3bdb740812d23bdd6d9
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2023-04-16 18:11:07 +0600

    Update Extentions + ViewsForForgotPasswordViewController.swift

--- RickAndMorty/Extentions + Views/Extentions + ViewsForForgotPasswordViewController.swift
+++ RickAndMorty/Extentions + Views/Extentions + ViewsForForgotPasswordViewController.swift
@@ -7,18 +7,10 @@
 
 import UIKit
 
-private let confirmPassword = UITextField(frame:
-                                            CGRect(x: 20, y: 250,
-                                                   width: 400, height: 40))
-private let labelWarning = UILabel(frame:
-                                    CGRect(x: 170, y: 100,
-                                           width: 200, height: 40))
-private let labelDescription = UILabel(frame:
-                                        CGRect(x: 140, y: 140,
-                                               width: 200, height: 40))
-private let buttonCheck = UIButton(frame:
-                                    CGRect(x: 95, y: 570,
-                                           width: 250, height: 45))
+private let confirmPassword = UITextField()
+private let labelWarning = UILabel()
+private let labelDescription = UILabel()
+private let buttonCheck = UIButton()
 
 extension ForgotPasswordViewController {
     
@@ -30,20 +22,47 @@ extension ForgotPasswordViewController {
         
         confirmPassword.placeholder = "X X X X "
         confirmPassword.borderStyle = .roundedRect
+        confirmPassword.translatesAutoresizingMaskIntoConstraints = false
+        confirmPassword.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        confirmPassword.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        confirmPassword.topAnchor.constraint(equalTo: view.topAnchor, constant: 250).isActive = true
+        confirmPassword.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        confirmPassword.widthAnchor.constraint(equalToConstant: 400).isActive = true
         
         labelWarning.text = "Warning!"
-        labelWarning.tintColor = .black
+        labelWarning.textColor = .red
         labelWarning.font = UIFont.systemFont(ofSize: 30)
+        labelWarning.translatesAutoresizingMaskIntoConstraints = false
+        labelWarning.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -25).isActive = true
+        labelWarning.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 25).isActive = true
+        labelWarning.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        labelWarning.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        labelWarning.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        labelWarning.textAlignment = .center
         
-        labelDescription.text = "if you forget account password, you can resume with Phone Number"
-        labelDescription.numberOfLines = 0
+        labelDescription.text = "if you forget account password\n you can resume with Phone Number"
+        labelDescription.textColor = .lightGray
+        labelDescription.numberOfLines = 2
         labelDescription.lineBreakMode = .byWordWrapping
         labelDescription.tintColor = .lightGray
+        labelDescription.translatesAutoresizingMaskIntoConstraints = false
+        labelDescription.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        labelDescription.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        labelDescription.topAnchor.constraint(equalTo: view.topAnchor, constant: 140).isActive = true
+        labelDescription.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        labelDescription.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        labelDescription.textAlignment = .center
         
         buttonCheck.layer.cornerRadius = 10
         buttonCheck.setTitle("Confirm", for: .normal)
         buttonCheck.backgroundColor = .blue
         buttonCheck.addTarget(self, action: #selector(check), for: .touchUpInside)
+        buttonCheck.translatesAutoresizingMaskIntoConstraints = false
+        buttonCheck.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -95).isActive = true
+        buttonCheck.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        buttonCheck.topAnchor.constraint(equalTo: view.topAnchor, constant: 570).isActive = true
+        buttonCheck.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        buttonCheck.widthAnchor.constraint(equalToConstant: 400).isActive = true
         
     }
     @objc
