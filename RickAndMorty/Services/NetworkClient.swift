47daa16f537357c27737a5980816356d45661827
UrOkaSan <tashtanaliev11@mail.ru>
2023-04-26 19:07:29 +0600

    Network Service added

--- RickAndMorty/Services/NetworkClient.swift
+++ RickAndMorty/Services/NetworkClient.swift
@@ -5,4 +5,28 @@
 //  Created by Boris Dobretsov on 12/4/23.
 //
 
-import Foundation
+import UIKit
+
+class NetworkLayer {
+    static let shared = NetworkLayer()
+    
+    func decode<T: Decodable>(with data: Data) -> T? {
+        try? JSONDecoder().decode(T.self, from: data)
+    }
+    private var baseURL = URL(string: "ds")
+    
+    func fetch(with data: Data, completion: @escaping (Result<String,Error>) -> Void) {
+        let request = URLRequest(url: baseURL!)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                completion(.failure(error))
+            }
+            if data != nil {
+                completion(.success("Got data"))
+            }
+        }
+        .resume()
+    }
+}
+
